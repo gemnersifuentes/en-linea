d17dedd341b768622f823c84ccde6659c48d80f6
sifuentesgemner <sifuentesgemner@gmail.com>
2024-10-06 10:27:05 -0500

    en-linea

---
+++ edicion.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>En linia | edicion</title>
+    <?php include 'partes/css.php' ?>
+</head>
+
+<body class="px-3">
+
+    <?php include 'partes/header.php' ?>
+    <div class="container-md mt-5 mb-5">
+        <div class="card border-0">
+
+            <div class="card-header bg-white border-0">
+                <h3 class="card-title"><i class="bi bi-file-plus-fill"></i>EDICION DE LIBROS</h3>
+            </div>
+            <div class="card-body">
+                <div class="row">
+                    <div class="col-sm-12 col-md-6">
+                        <img src="resources/img/idea_03.PNG" class="w-100 h-90" alt="...">
+                    </div>
+                    <div class="col-sm-12 col-md-6">
+                        <p class="card-text" >
+
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, optio dolorum sequi, velit ab voluptatibus minus natus beatae facere modi ex! Impedit, velit molestias magnam voluptatibus eaque provident ut cum! Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo voluptas exercitationem, fugit blanditiis tenetur error, ad veniam dolorum labore est, enim qui fugiat aliquid eos expedita sint soluta laboriosam quisquam.lore Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, optio dolorum sequi, velit ab voluptatibus minus natus beatae facere modi ex! Impedit, velit molestias magnam voluptatibus eaque provident ut cum! Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo voluptas exercitationem, fugit blanditiis tenetur error, ad veniam dolorum labore est, enim qui fugiat aliquid eos expedita sint soluta laboriosam quisquam.lore Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, optio dolorum sequi, velit ab voluptatibus minus natus beatae facere modi ex! Impedit, velit molestias magnam voluptatibus eaque provident ut cum! Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo voluptas exercitationem, fugit blanditiis tenetur error, ad veniam dolorum labore est, enim qui fugiat aliquid eos expedita sint soluta laboriosam quisquam.lore</p>
+
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+
+    </div>
+
+
+
+    <?php include 'partes/footer.php' ?>
+
+</body>
+
+</html>
---
+++ partes/header.php
@@ -0,0 +1,39 @@
+<div class="row mt-4 mb-2">
+    <div class="col-sm-12 col-md-6">
+
+        <div class="row">
+            <div class="col-sm-12 col-md-12 d-flex align-items-center">
+                <img src="resources/img/logo.png" class=" ml-5" style="width: 45px; height: 27px" alt="...">
+                <h2 class="h2">EN LÍNEA</h2>
+            </div>
+        </div>
+    </div>
+
+
+    <div class="col-sm-12 col-md-6">
+        <p class="text-right pr-5 mt-2    ">SOLUCIONES MUNDIALES</p>
+    </div>
+</div>
+<div class="row">
+
+
+    <div class="col-sm-12">
+
+
+        <div class="card">
+            <img src="resources/img/baner01.png" style="height: 500px;" class="w-100" alt="">
+            <div class="card-img-overlay mt-5 d-flex align-items-center">
+                <div>
+                    <h3 class="bg-wine text-white px-4  d-inline">¿NECESITAS</h3>
+                    <h3 class="bg-wine text-white pl-4 mt-2">AYUDA? ¡NUNCA HABIA SIDO</h3>
+                    <h3 class="bg-wine text-white pl-4 ">TAN FACIL SER RECONOCIDO!</h3>
+
+                    <p class=" bg-dark text-white pl-4  mb-1">permitenos demostrarte lo que nuestro equipo</p>
+                    <p class=" bg-dark text-white px-4 d-inline ">puede hacer por tu negocio</p>
+                </div>
+
+            </div>
+        </div>
+
+    </div>
+</div>
